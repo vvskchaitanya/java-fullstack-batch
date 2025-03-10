32db6f3c4f9306f2dda9ce062a6b0ab5ec3a3f4f
vvskchaitanya <vvskchaitanya@gmail.com>
2025-03-10 02:55:13 +0000

    updated thread example

--- chaitanya/src/com/fullstack/java/threads/MyThread.java
+++ chaitanya/src/com/fullstack/java/threads/MyThread.java
@@ -5,24 +5,34 @@ package com.fullstack.java.threads;
  */
 public class MyThread extends Thread{
 
-    public MyThread(String name){
+    private int start;
+
+    private int end;
+
+    public MyThread(String name, int start, int end){
         super(name);
+        this.start =start;
+        this.end = end;
         System.out.println("Thread is in new / intialized state: "+this.getName());
     }
 
     public void run(){
         System.out.println("Thread is in running state: "+this.getName());
+
+        for(int i=start;i<=end;i++){
+            System.out.println(this.getName()+" "+i);
+        }
     }
 
     public static void main(String[] args) {
         // Create Thread
-        MyThread thread1 = new MyThread("MyThread1");
+        MyThread thread1 = new MyThread("MyThread1",1,5);
 
-        MyThread thread2 = new MyThread("MyThread2");
+        MyThread thread2 = new MyThread("MyThread2",10,15);
 
-        MyThread thread3 = new MyThread("MyThread3");
+        MyThread thread3 = new MyThread("MyThread3",20,25);
 
-        MyThread thread4 = new MyThread("MyThread4");
+        MyThread thread4 = new MyThread("MyThread4",30,35);
 
         thread1.start();
 
