ecc56b9e7a292a636275042f4fcf3950b0eb21bf
Triveniinturi <triveniinturi7@gmail.com>
2025-03-22 01:08:05 -0500

    Adding maven project

---
+++ Triveni/src/maven/my-first-maven-project/src/main/java/com/triveni/fullstack/App.java
@@ -0,0 +1,13 @@
+package com.triveni.fullstack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello, This is my first Java project!" );
+    }
+}
